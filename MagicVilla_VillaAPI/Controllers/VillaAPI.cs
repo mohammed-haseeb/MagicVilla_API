990cbae70cf9341a6a47730b62f093c060d1855b
Mohammed Haseeb <mdhaseeb.98@gmail.com>
2024-05-21 15:10:36 +0530

    created the first endpoint (get)

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPI.cs
@@ -0,0 +1,23 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+	[Route("api/VillaAPI")]
+	[ApiController]
+	public class VillaAPI : ControllerBase
+	{
+		[HttpGet]
+		public IEnumerable<VillaDTO> GetVillas()
+		{
+			return VillaStore.villaList;
+		}
+
+		[HttpGet("{id:int}")]
+		public VillaDTO GetVilla(int id)
+		{
+			return VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+		}
+	}
+}
