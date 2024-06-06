af5c0334d8569342a6a196e7601d347273760bc4
Mohammed Haseeb <mdhaseeb.98@gmail.com>
2024-06-06 14:41:25 +0530

    Implemented Base and Villa Service

---
+++ MagicVilla_Web/Services/IServices/IVillaService.cs
@@ -0,0 +1,13 @@
+﻿using MagicVilla_Web.Models.Dto;
+
+namespace MagicVilla_Web.Services.IServices
+{
+	public interface IVillaService
+	{
+		Task<T> GetAllAsync<T>();
+		Task<T> GetAsync<T>(int id);
+		Task<T> CreateAsync<T>(VillaCreateDTO createDto);
+		Task<T> UpdateAsync<T>(VillaUpdateDTO updateDto);
+		Task<T> DeleteAsync<T>(int id);
+	}
+}
